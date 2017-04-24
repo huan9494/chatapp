27d8bf3e8c6eaed9bca6d93f13a2f20a3e048965
huan <huan20794@gmail.com>
2017-04-25 04:22:14 +0700

    delete foreign true

--- db/migrate/20170420190508_create_chatroom_users.rb
+++ db/migrate/20170420190508_create_chatroom_users.rb
@@ -1,8 +1,8 @@
 class CreateChatroomUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :chatroom_users do |t|
-      t.references :chatroom, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :chatroom
+      t.references :user
 
       t.timestamps
     end
--- db/migrate/20170420190546_create_messages.rb
+++ db/migrate/20170420190546_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.references :chatroom, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :chatroom
+      t.references :user
       t.text :body
 
       t.timestamps
